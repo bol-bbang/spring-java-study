419fe6fe52730425b0c2822dc15bed1cdaaba93b
Green_박화희 <hhpark@maxst.com>
2023-11-26 21:15:13 +0900

    [event-handling] user GeneratedValue 추가

--- event-handling/src/main/java/com/example/service/user/entity/User.java
+++ event-handling/src/main/java/com/example/service/user/entity/User.java
@@ -1,6 +1,7 @@
 package com.example.service.user.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -12,6 +13,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class User {
 	@Id
+	@GeneratedValue
 	private Long id;
 
 	private String email;
