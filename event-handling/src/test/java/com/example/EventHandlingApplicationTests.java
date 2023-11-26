856e1a9cd8a19f04fda42d99f8da4946bcc74d15
Green_박화희 <hhpark@maxst.com>
2023-11-26 22:43:39 +0900

    [event-handling] 메일발송 에러발생시 sync, async차이 테스트

--- event-handling/src/test/java/com/example/EventHandlingApplicationTests.java
+++ event-handling/src/test/java/com/example/EventHandlingApplicationTests.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 class EventHandlingApplicationTests {
@@ -28,11 +29,45 @@ class EventHandlingApplicationTests {
 		assertThat(all.get(0).getEmail()).isEqualTo(email);
 	}
 	@Test
+	void 유저저장_실패_sync() {
+		String email = "user-save@fail.com";
+		String name = "green";
+		// 유저저장 실패시 exception 발생
+		assertThatThrownBy(() -> userService.signUp(email, name));
+	}
+	@Test
+	void 메일발송_실패_sync() {
+		String email = "send-email@fail.com";
+		String name = "green";
+		// 메일발송 실패시 exception 발생
+		assertThatThrownBy(() -> userService.signUp(email, name));
+	}
+	@Test
 	void 가입메일발송_async() {
 		String email = "send@test.com";
 		String name = "green";
 		userService.signUp_asyncSendEmail(email, name);
 
+		List<User> all = userService.getUsers();
+		assertThat(all.size()).isEqualTo(1);
+		assertThat(all.get(0).getEmail()).isEqualTo(email);
+	}
+
+	@Test
+	void 유저저장_실패_async() {
+		String email = "user-save@fail.com";
+		String name = "green";
+		// 유저저장 실패시 exception 발생
+		assertThatThrownBy(() -> userService.signUp_asyncSendEmail(email, name));
+	}
+	@Test
+	void 메일발송_실패_async() {
+		String email = "send-email@fail.com";
+		String name = "green";
+		userService.signUp_asyncSendEmail(email, name);
+
+		// 메일발송 실패시 exception 발생 안함.
+		// 유저정보 정상 저장완료.
 		List<User> all = userService.getUsers();
 		assertThat(all.size()).isEqualTo(1);
 		assertThat(all.get(0).getEmail()).isEqualTo(email);
