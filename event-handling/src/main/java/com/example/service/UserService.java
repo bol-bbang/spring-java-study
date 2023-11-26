40ad9885601408c2b0cbd9265646059e5a437e7f
Green_박화희 <hhpark@maxst.com>
2023-11-26 13:19:49 +0900

    [event-handling] 사용자 생성로직 메서드로 분리

--- event-handling/src/main/java/com/example/service/UserService.java
+++ event-handling/src/main/java/com/example/service/UserService.java
@@ -2,9 +2,11 @@ package com.example.service;
 
 import com.example.event.SignUpEvent;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserService {
@@ -12,8 +14,13 @@ public class UserService {
 	private final ApplicationEventPublisher publisher;
 	public void signUp(String email, String name) {
 		//1.유저저장
-		//repository.save(user);
+		createUser(email, name);
 		//2.메일발송
 		publisher.publishEvent(new SignUpEvent(email, name));
+	}
+
+	private void createUser(String email, String name) {
+		//repository.save(user);
+		log.info("유저 저장, email: {}, name: {}", email, name);
 	}
 }
